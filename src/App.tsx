a5f799acbd3d02cd3dfe21a2166ec17b78c52d2a
Kkasya <kasya--k@yandex.by>
2021-03-22 18:54:01 +0300

    feat: add data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import './App.scss';
-import Tutorial from "./components/tutorial";
 
 function App() {
   return (
       <>
     <div>Hello, RS Lang</div>
-      <Tutorial />
       </>
   );
 }
