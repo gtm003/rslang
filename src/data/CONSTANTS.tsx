e3ac69c5dc83867d5a80348e78aa1bf096226c28
Kkasya <kasya--k@yandex.by>
2021-03-23 20:02:47 +0300

    feat: add types for constants

--- src/data/CONSTANTS.tsx
+++ src/data/CONSTANTS.tsx
@@ -1,4 +1,4 @@
-const githubMembers = ['Alekseishkel', 'Kkasya', "Alexk08", '11alexey11', 'gtm003'];
-const githubSrc = 'https://github.com/';
+const githubMembers: string[] = ['Alekseishkel', 'Kkasya', "Alexk08", '11alexey11', 'gtm003'];
+const githubSrc: string = 'https://github.com/';
 
 export {githubMembers, githubSrc};
