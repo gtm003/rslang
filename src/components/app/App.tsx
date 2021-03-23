a3bce9f49e92e9053d86de2686e918d2435f5917
Kkasya <kasya--k@yandex.by>
2021-03-23 18:31:13 +0300

    fix: return to init app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import './App.scss';
-import {Footer} from "../footer";
 
 function App() {
   return (
     <>
       <div>Hello, RS Lang</div>
-      <Footer />
     </>
   );
 }
