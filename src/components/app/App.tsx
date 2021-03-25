3f4dcc64c3961a6d64dc6d2c9c6c8407b70678aa
Grigorovich Tatyana <gtm003@gmail.com>
2021-03-25 23:57:45 +0300

    fix: correct App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import { Footer } from '../footer';
 import { Header } from '../header';
-import { GameSprint } from '../sprint';
 
 function App() {
   return (
     <>
       <Header />
       <Footer isAuth={false}/>
-      <GameSprint page={0} group={0} />
     </>
   );
 }
