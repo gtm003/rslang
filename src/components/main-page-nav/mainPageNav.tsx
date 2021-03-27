5fe0fadd6e28e0e4bfeef2c62d511216538e290d
alekseishkel <lexashkel@gmail.com>
2021-03-27 13:23:03 +0300

    fix: move main page nav to separate component

---
+++ src/components/main-page-nav/mainPageNav.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const MainPageNav: React.FC = () => {
+  let mainPageNav: string[];
+  const isAuth: boolean = false;
+
+  isAuth ?
+    mainPageNav = ["Учебник", "Мини-игры", "Словарь", "Статистика", "Настройка"] :
+    mainPageNav = ["Учебник", "Мини-игры", "Статистика", "Настройка"];
+
+  return (
+    <nav className="main-page-nav">
+      <ul className="main-page-nav__list">
+        {mainPageNav.map((item) => {
+          return (
+            <li className="main-page-nav__item">
+              <a href="/">
+                {item}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+export {MainPageNav};
--- src/components/main-page/mainPage.tsx
+++ src/components/main-page/mainPage.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-const MainPage = () => {
-  let mainPageNav: string[];
-  const isAuth: boolean = false;
+import {MainPageNav} from '../main-page-nav';
 
-  isAuth ?
-    mainPageNav = ["Учебник", "Мини-игры", "Словарь", "Статистика", "Настройка"] :
-    mainPageNav = ["Учебник", "Мини-игры", "Статистика", "Настройка"];
+const MainPage = () => {
+  
 
   return (
     <main className="main-page">
@@ -22,19 +19,7 @@ const MainPage = () => {
         <button className="main-page__video-btn btn">Смотреть видео</button>
       </section>
       <div className="main-page__bg-wrapper">
-        <nav className="main-page__nav">
-          <ul className="main-page__nav-list">
-            {mainPageNav.map((item) => {
-              return (
-                <li className="main-page__nav-item">
-                  <a href="/">
-                    {item}
-                  </a>
-                </li>
-              )
-            })}
-          </ul>
-        </nav>
+        <MainPageNav />
       </div>
     </main>
   );
