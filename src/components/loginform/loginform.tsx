8ff014be7c07b079eb0d2817710a6159a8620fb6
Alexey Yanvarev <yanvarev_2015@mail.ru>
2021-03-30 10:30:13 +0400

    feat: add styles for login and signup forms

--- src/components/loginform/loginform.tsx
+++ src/components/loginform/loginform.tsx
@@ -6,23 +6,25 @@ const LoginForm: React.FC = (props: any) => {
     const handleSubmit = props.handleSubmit;
     const submit = (values: any) => console.log(values);
     return (
-        <div className="form-wrapper">
-            <form className="form" onSubmit={handleSubmit(submit)}>
-                <div className="form-fields">
-                    <div className="form-fields__item">
-                        <label htmlFor="email">Email</label>
-                        <Field name="email" component="input" type="email" required={true} />
-                    </div>
-                    <div className="form-fields__item">
-                        <label>Пароль</label>
-                        <Field name="password" component="input" type="password" required={true} />
-                    </div>
-                    <div className="form-fields__item">
-                        <Link to="/sign-up">Нет аккаунта? Зарегистрируйтесь!</Link>
-                    </div>
+        <div className="login-form-wrapper">
+            <form className="login-form" onSubmit={handleSubmit(submit)}>
+                <div className="login-form-title">
+                    <h2>Изучать слова удобнее, если у вас есть профиль</h2>
                 </div>
-                <div className="form-submit">
-                    <button className="btn" type="submit">Войти</button>
+                <div className="login-form-subtitle">
+                    <span>Вы получите доступ к долгосрочному хранению статистики, а также сможете формировать собственный словарь.</span>
+                </div>
+                <div className="login-form-fields">
+                    <Field className="login-form-fields__item" name="email" component="input" type="email" placeholder="E-mail" required={true} />
+                    <Field className="login-form-fields__item" name="password" component="input" type="password" placeholder="Пароль" required={true} />
+                </div>
+                <div className="login-form-submit">
+                    <button className="login-form-submit__btn" type="submit">Войти</button>
+                </div>
+                <div>
+                    <div>
+                        Забыли пароль? | <Link to="/sign-up">Регистрация</Link>
+                    </div>
                 </div>
             </form>
         </div>
--- src/components/signupform/signupform.tsx
+++ src/components/signupform/signupform.tsx
@@ -6,31 +6,22 @@ const SignUpForm: React.FC = (props: any) => {
     const handleSubmit = props.handleSubmit;
     const submit = (values: any) => console.log(values);
     return (
-        <div className="form-wrapper">
-            <form className="form" onSubmit={handleSubmit(submit)}>
-                <div className="form-fields">
-                    <div className="form-fields__item">
-                        <label htmlFor="name">Имя</label>
-                        <Field name="name" component="input" type="text" required={true} />
-                    </div>
-                    <div className="form-fields__item">
-                        <label htmlFor="email">Email</label>
-                        <Field name="email" component="input" type="email" required={true} />
-                    </div>
-                    <div className="form-fields__item">
-                        <label htmlFor="password">Пароль</label>
-                        <Field name="password" component="input" type="password" required={true} />
-                    </div>
-                    <div className="form-fields__item">
-                        <label htmlFor="photo">Фото</label>
-                        <Field name="photo" component="input" type="file" accept=".jpg, .jpeg, .png" />
-                    </div>
-                    <div className="form-fields__item">
-                        <Link to="/sign-up">Нет аккаунта? Зарегистрируйтесь!</Link>
-                    </div>
+        <div className="signup-form-wrapper">
+            <form className="signup-form" onSubmit={handleSubmit(submit)}>
+                <div className="signup-form-title">
+                    <h2>Изучать слова удобнее, если у вас есть профиль</h2>
                 </div>
-                <div className="form-submit">
-                    <button className="btn" type="submit">Войти</button>
+                <div className="signup-form-subtitle">
+                    <span>Вы получите доступ к долгосрочному хранению статистики, а также сможете формировать собственный словарь.</span>
+                </div>
+                <div className="signup-form-fields">
+                    <Field className="signup-form-fields__item" name="name" component="input" type="text" placeholder="Имя" required={true} />
+                    <Field className="signup-form-fields__item" name="email" component="input" type="email" placeholder="E-mail" required={true} />
+                    <Field className="signup-form-fields__item" name="password" component="input" type="password" placeholder="Пароль" required={true} />
+                    <Field className="signup-form-fields__item" name="photo" component="input" type="file" accept=".jpg, .jpeg, .png" />
+                </div>
+                <div className="signup-form-submit">
+                    <button className="signup-form-submit__btn" type="submit">Создать аккаунт</button>
                 </div>
             </form>
         </div>
