61f2e89c4fa17e09d22ec19239abe5c3619d9657
Aleksandr <akapacuk@gmail.com>
2021-04-03 21:48:36 +0400

    feat: correct some styles for wrapper, footer, header

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -12,7 +12,9 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <HeaderRedux isAuth={false}/>
-      <Switcher />
+      <div className="main-wrapper">
+        <Switcher />
+      </div>
       <Settings />
       <LoginFormRedux />
       <SignUpFormRedux />
