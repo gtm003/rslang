7e269a471d645906cbac4a3ce6d4e63be33a59c2
Kkasya <kasya--k@yandex.by>
2021-04-03 23:16:46 +0300

    feat: add link from side-menu to sign-up

--- src/components/side-menu/side-menu.tsx
+++ src/components/side-menu/side-menu.tsx
@@ -4,15 +4,17 @@ import { NavLink } from 'react-router-dom';
 import { navLink, menuLinks } from '../../data';
 import {toggleOpen} from "../../common/redux/action";
 import {connect} from "react-redux";
+import {toggleLoginOpen} from "../../common/redux/login-action-creator";
 
 interface SideMenuProps {
   isAuth: boolean
   isMenuOpen: boolean
   onMenuLinkClick: () => void,
   toggleOpen: any,
+  toggleLoginOpen: (isLoginOpen: boolean) => void,
 }
 
-const SideMenuRedux: React.FC<SideMenuProps> = ({isAuth, isMenuOpen, onMenuLinkClick, toggleOpen }) => {
+const SideMenuRedux: React.FC<SideMenuProps> = ({isAuth, isMenuOpen, onMenuLinkClick, toggleOpen, toggleLoginOpen }) => {
   const clazz = isAuth ? 'side-menu--auth' : '';
   const isVisible = isMenuOpen ? 'side-menu--show' : '';
 
@@ -29,12 +31,9 @@ const SideMenuRedux: React.FC<SideMenuProps> = ({isAuth, isMenuOpen, onMenuLinkC
           return (
             <li className={`side-menu__item ${clazz}`} key={item}>
               {(item === 'Настройка') ?
-                <span onClick={() => {
-                  onMenuLinkClick();
-                  toggleOpen(true);
-                }}>{item}</span> :
+                <span onClick={() => { onMenuLinkClick(); toggleOpen(true)}}>{item}</span> :
                 <NavLink to={`/${navLink[item]}`}
-                         onClick={onMenuLinkClick}
+                         onClick = {(item === 'Войти') ?  () => {onMenuLinkClick(); toggleLoginOpen(true)} :  onMenuLinkClick}
                          activeClassName='active'>
                   {item}
                 </NavLink>}
@@ -46,9 +45,12 @@ const SideMenuRedux: React.FC<SideMenuProps> = ({isAuth, isMenuOpen, onMenuLinkC
   )
 }
 
-const mapDispatchToProps = {
+const mapDispatchToProps = (dispatch: any) => ({
   toggleOpen,
-};
+  toggleLoginOpen: (isLoginOpen: boolean) => {
+    dispatch(toggleLoginOpen(isLoginOpen));
+  },
+});
 
 const SideMenu = connect(null, mapDispatchToProps)(SideMenuRedux);
 export {SideMenu};
