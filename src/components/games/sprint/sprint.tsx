a653cd66e0dd00896d8fcdf3ca8d93e27cb25fae
Grigorovich Tatyana <gtm003@gmail.com>
2021-04-04 20:48:13 +0300

    fix: correct conflict

--- src/components/games/sprint/sprint.tsx
+++ src/components/games/sprint/sprint.tsx
@@ -35,23 +35,6 @@ interface GameSprintProps {
   group: number,
   page?: number,
 }
-/*
-interface WordsProps {
-  "id": "string",
-  "group": 0,
-  "page": 0,
-  "word": "string",
-  "image": "string",
-  "audio": "string",
-  "audioMeaning": "string",
-  "audioExample": "string",
-  "textMeaning": "string",
-  "textExample": "string",
-  "transcription": "string",
-  "wordTranslate": "string",
-  "textMeaningTranslate": "string",
-  "textExampleTranslate": "string"
-}*/
 
 const GameSprint: React.FC<GameSprintProps> = ({group, page}) => {
   //const [score, setScore] = useState<number>(0);
