1310734c3ef8357ed237f391fecc436d43ec3869
Alexey Yanvarev <yanvarev_2015@mail.ru>
2021-04-05 01:39:19 +0400

    feat: add localstorage saving for user

--- src/common/redux/login-action-creator.ts
+++ src/common/redux/login-action-creator.ts
@@ -1,14 +1,23 @@
 const toggleLoginOpen = (isLoginOpen: boolean) => ({ type: 'TOGGLE_LOGIN_OPEN', payload: isLoginOpen });
 
-const loginUser = (name: string | null, userId: string | null, photo: string | null, isAuth: boolean) => ({
-    type: 'LOGIN_USER',
-    payload: {
+const loginUser = (name: string | null, userId: string | null, photo: string | null, isAuth: boolean) => {
+    localStorage.user = JSON.stringify({
         name,
         userId,
         photo,
         isAuth,
+    });
+
+    return {
+        type: 'LOGIN_USER',
+        payload: {
+            name,
+            userId,
+            photo,
+            isAuth,
+        }
     }
-});
+};
 
 const setLoginError = (error: string | null) => ({ type: 'SET_LOGIN_ERROR', payload: error });
 
