555f033aa773c22f3a1b699952fd7b282d1e9d14
alekseishkel <lexashkel@gmail.com>
2021-04-06 21:12:35 +0300

    fix: fix some bugs

--- src/components/games/savannah/savannah.tsx
+++ src/components/games/savannah/savannah.tsx
@@ -7,7 +7,6 @@ import { ResultPercent } from '../resultPercent/resultPercent';
 import { ResultWordsList } from '../ResultWordsList/resultWordsList';
 import { NavLink } from 'react-router-dom';
 
-// let lives = 5;
 let correctAnswers: WordsProps[] = [];
 let wrongAnswers: WordsProps[] = [];
 
@@ -19,15 +18,17 @@ const SavannahRedux: React.FC<GameProps & SavannahProps> = ({ group, page, words
   const [gameWords, setGameWords] = useState<WordsProps[]>([]);
   const [translations, setTranslations] = useState<WordsProps[]>([]);
   const [lives, setLives] = useState<number>(5);
-  const [isPlay, setIsPlay] = useState<boolean>(true);
+  const [isPlay, setIsPlay] = useState<boolean>(false);
   const [listResultsNumber, setListResultsNumber] = useState<number>(0);
-  // const [correctAnswers, setCorrectAnswers] = useState<WordsProps[]>([]);
-  // const [wrongAnswers, setWrongAnswers] = useState<WordsProps[]>([]);
   const word = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    setIsPlay(true);
+  }, [words])
+
+  useEffect(() => {
     if (isPlay) {
-      const necessaryArray = words.slice((Number(group) * 600), ((Number(group) + 1) * 600));
+      const necessaryArray = words.slice((Number(group) * 600), 4);
       setGameWords(necessaryArray);
       setTranslations(necessaryArray);
       correctAnswers = [];
@@ -35,31 +36,18 @@ const SavannahRedux: React.FC<GameProps & SavannahProps> = ({ group, page, words
     }
   }, [words, group, isPlay]);
 
-
-  // useEffect(() => {
-  //   setTranslations(gameWords.map((word) => word.wordTranslate));
-  // }, [gameWords])
-
   useEffect(() => {
-    if (lives <= 0) {
+
+    // bug is here
+      if (isPlay && (lives <= 0 || (lives > 0 && gameWords.length === 0))) {
+        console.log(1);
+        
       setIsPlay(false);
     }
-  }, [lives])
-
-  // useEffect(() => {
-  //   getDataPage(group, page).then(res => {
-  //     setWords(res);
-  //     setTranslations(res.map((word) => word.wordTranslate));
-  //   });
-  // }, []);
+  }, [lives, gameWords, isPlay])
 
   const shuffleArray = (array: WordsProps[]) => {
-    for (let i = 0; i <= array.length - 1; i++) {
-      let j = Math.floor(Math.random() * i);
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-
-    return array;
+    return array.sort(() => Math.random() - 0.5);
   };
 
   const onAnswer = (wordTranslation: WordsProps, evt?: any): void => {
@@ -72,25 +60,28 @@ const SavannahRedux: React.FC<GameProps & SavannahProps> = ({ group, page, words
       translation.lastChild?.textContent === wordTranslation.wordTranslate ?
         translation.classList.add("word-correct") :
         translation.classList.add("word-wrong");
-    })
-
-    setTimeout(() => {
-      if (wrongAnswer) {
+      })
+      
+      setTimeout(() => {
+        if (wrongAnswer) {
         setLives(prevLives => prevLives - 1);
         wrongAnswers.push(wordTranslation);
-        // setWrongAnswers((prevAnswers) => [...prevAnswers, wordTranslation]);
       } else {
         correctAnswers.push(wordTranslation);
-
-        // setCorrectAnswers((prevAnswers) => [...prevAnswers, wordTranslation]);
       }
 
-      const updatedWords = gameWords.filter((word) => word.word !== wordForTranslation);
+      const updatedWords = gameWords.filter((word) => word.word !== wordTranslation.word);
       setGameWords(updatedWords);
 
       if (word.current) {
         word.current.style.animationPlayState = "running";
       }
+
+      wordTranslations?.forEach((translation) => {
+        translation.lastChild?.textContent === wordTranslation.wordTranslate ?
+          translation.classList.remove("word-correct") :
+          translation.classList.remove("word-wrong");
+        })
 
       word.current?.classList.replace("flow-animation", "word-hidden");
       setTimeout(() => word.current?.classList.replace("word-hidden", "flow-animation"), 50);
@@ -98,22 +89,17 @@ const SavannahRedux: React.FC<GameProps & SavannahProps> = ({ group, page, words
   };
 
   const restartGame = () => {
+    console.log(3);
+    
     setIsPlay(true);
-    // setCorrectAnswers([]);
-    // setWrongAnswers([]);
     setLives(5);
   }
 
-  if (gameWords.length === 0 || translations.length === 0) {
-    return <Loader />
-  }
-
   const translationWordIndex: number = Math.floor(Math.random() * gameWords.length);
-  const wordForTranslation: string = gameWords[translationWordIndex].word;
-  const wordTranslation: WordsProps = gameWords[translationWordIndex];
+  const wordForTranslation: WordsProps = gameWords[translationWordIndex] || {};
   const translationsCopy = translations.slice();
 
-  translationsCopy.splice(translations.findIndex((word) => word.wordTranslate === wordTranslation.wordTranslate), 1);
+  translationsCopy.splice(translations.findIndex((word) => word.wordTranslate === wordForTranslation.wordTranslate), 1);
 
   const getRandomWords = () => {
     const randomWordIndex: number = Math.floor(Math.random() * translationsCopy.length);
@@ -128,17 +114,21 @@ const SavannahRedux: React.FC<GameProps & SavannahProps> = ({ group, page, words
     getRandomWords(),
     getRandomWords(),
     getRandomWords(),
-    wordTranslation
+    wordForTranslation
   ]
+
+  if (gameWords.length === 0 && translations.length === 0) {
+    return <Loader />
+  }
 
   return (
     <main className="savannah">
-      {isPlay ?
+      {isPlay && gameWords.length !== 0?
         <>
           <Lives lives={lives} />
-          <div className="savannah__word flow-animation" ref={word} onAnimationEnd={() => { onAnswer(wordTranslation) }}>
+          <div className="savannah__word flow-animation" ref={word} onAnimationEnd={() => { onAnswer(wordForTranslation) }}>
             {
-              wordForTranslation
+              wordForTranslation.word
             }
           </div>
           <div className="savannah__translation">
@@ -146,7 +136,7 @@ const SavannahRedux: React.FC<GameProps & SavannahProps> = ({ group, page, words
               {
                 shuffleArray(wordsOnScreen).map((word: WordsProps, i: number) => {
                   return (
-                    <li key={word.wordTranslate + i} className="savannah__translation-item" onClick={evt => onAnswer(wordTranslation, evt)} >
+                    <li key={word.wordTranslate + i} className="savannah__translation-item" onClick={evt => onAnswer(wordForTranslation, evt)} >
                       <span>{i + 1}. </span>
                       <span>
                         {word.wordTranslate}
