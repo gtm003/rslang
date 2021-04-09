7218a65d5041378c146d83cf2984519e08ebf798
Grigorovich Tatyana <gtm003@gmail.com>
2021-04-09 17:24:57 +0300

    feat: add series-svg to the sprint component, add styles

--- src/components/games/resultPercent/resultPercent.tsx
+++ src/components/games/resultPercent/resultPercent.tsx
@@ -13,7 +13,7 @@ const ResultPercent: React.FC<ResultPercentProps> = ({error, correct}) => {
     <div className="percent">
       <div className="morph-shape" id="morph-shape"  >
         <svg xmlns="http://www.w3.org/2000/svg" transform = {`translate(0, ${110 - 1.5 * result(error, correct)})`}  width="100%" height="120%" viewBox="0 0 100 100"  preserveAspectRatio="none">
-          <path fill="#afafaf" d="M 0 0 C 0 0 20 20 33 20 C 45 20 55 0 67 0 C 78 0 100 20 100 20 C 100 20 100 100 100 100 L 0 100 Z">
+          <path fill="#00BF97" d="M 0 0 C 0 0 20 20 33 20 C 45 20 55 0 67 0 C 78 0 100 20 100 20 C 100 20 100 100 100 100 L 0 100 Z">
             <animate
               attributeName="d"
               dur="7s"
