21d45feab340646fdec0faf70098228f566e04a1
alekseishkel <lexashkel@gmail.com>
2021-04-12 17:26:53 +0300

    feat: add playing on sound icon click

--- src/components/games/audio-challenge/audio-challenge.tsx
+++ src/components/games/audio-challenge/audio-challenge.tsx
@@ -3,6 +3,7 @@ import { WordsProps, GameProps } from "../../../common/ts/interfaces";
 import { connect } from "react-redux";
 import { Loader } from "../../loader";
 import { ResultsGame } from '../resultsGame/resultsGame';
+import { urlBackend } from "../../../data";
 
 let necessaryWords: WordsProps[];
 let correctAnswers: WordsProps[] = [];
@@ -18,7 +19,6 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
   const [lives, setLives] = useState<number>(5);
   const savannah = useRef<HTMLElement>(null);
   const word = useRef<HTMLDivElement>(null);
-  const sun = useRef<HTMLImageElement>(null);
 
   useEffect(() => {
     page > -1 ?
@@ -36,6 +36,16 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
     window.addEventListener("keyup", onKeyUpHandler);
     return () => (window as any).removeEventListener("keyup", onKeyUpHandler);
   }, [gameWords]);
+
+  // useEffect(() => {
+  //   playAudio(translationWord.audio);
+  // }, []);
+
+  const playAudio = (audioSrc: string): void => {
+    const audio = new Audio();
+    audio.src = urlBackend + audioSrc;
+    audio.play();
+  }
 
   const onKeyUpHandler = (evt: KeyboardEvent) => {
     switch (evt.key) {
@@ -167,10 +177,8 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
                 </svg>
               </button>
             </div>
-            <div className="savannah__word flow-animation" ref={word} onAnimationEnd={() => { onAnswer(translationWord) }}>
-              {
-                translationWord.word
-              }
+            <div className="audio-challenge__word" ref={word} onClick={() => playAudio(translationWord.audio)}>
+              <svg width="128" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><title /><g data-name="Layer 33" id="Layer_33"><path className="cls-1" d="M18,29a1,1,0,0,1-.57-.18l-10-7A1,1,0,0,1,7,21V11a1,1,0,0,1,.43-.82l10-7a1,1,0,0,1,1-.07A1,1,0,0,1,19,4V28a1,1,0,0,1-.54.89A1,1,0,0,1,18,29ZM9,20.48l8,5.6V5.92l-8,5.6Z" /><path className="cls-1" d="M8,22H4a3,3,0,0,1-3-3V13a3,3,0,0,1,3-3H8a1,1,0,0,1,1,1V21A1,1,0,0,1,8,22ZM4,12a1,1,0,0,0-1,1v6a1,1,0,0,0,1,1H7V12Z" /><path className="cls-1" d="M18,21V19a3,3,0,0,0,2.12-5.12l1.42-1.42A5,5,0,0,1,18,21Z" /><path className="cls-1" d="M23.65,22.65a1,1,0,0,1-.7-.29A1,1,0,0,1,23,21a7,7,0,0,0,0-9.9,1,1,0,0,1,1.41-1.41,9,9,0,0,1,0,12.72A1,1,0,0,1,23.65,22.65Z" /></g></svg>
             </div>
             <ul className="minigames__translation-list">
               {
