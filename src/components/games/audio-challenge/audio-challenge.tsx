424b1101d946ad431016fb51cfded1fc1c74e404
alekseishkel <lexashkel@gmail.com>
2021-04-14 16:32:08 +0300

    feat: add all game styles

--- src/components/games/audio-challenge/audio-challenge.tsx
+++ src/components/games/audio-challenge/audio-challenge.tsx
@@ -9,6 +9,8 @@ import { urlBackend } from "../../../data";
 let necessaryWords: WordsProps[];
 let correctAnswers: WordsProps[] = [];
 let wrongAnswers: WordsProps[] = [];
+let lives: number = 5;
+let onNextQuestionClick: number = 0;
 
 interface SavannahProps {
   words: WordsProps[]
@@ -17,12 +19,14 @@ interface SavannahProps {
 const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page = -1, words }) => {
   const [gameWords, setGameWords] = useState<WordsProps[]>([]);
   const [translations, setTranslations] = useState<WordsProps[]>([]);
-  const [lives, setLives] = useState<number>(5);
-  const [isAnswered, setIsAnswered] = useState<boolean>(false);
+  // const [lives, setLives] = useState<number>(5);
+  // const [isAnswered, setIsAnswered] = useState<boolean>(false);
   const [isWelcomeScreen, setIsWelcomeScreen] = useState<boolean>(true);
   const audioChallenge = useRef<HTMLElement>(null);
-  const soundWaves = useRef<SVGSVGElement>(null);
-  const word = useRef<HTMLDivElement>(null);
+  const soundWaves = useRef<HTMLImageElement>(null);
+  const wordContainer = useRef<HTMLDivElement>(null);
+  const word = useRef<HTMLSpanElement>(null);
+  const button = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     page > -1 ?
@@ -81,7 +85,7 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
       necessaryWords = words.slice((Number(group) * 600), ((Number(group)) * 600) + ((page + 1) * 20)) :
       necessaryWords = words.slice((Number(group) * 600), ((Number(group) + 1) * 600));
     setGameWords(necessaryWords);
-    setLives(5);
+    lives = 5;
     correctAnswers = [];
     wrongAnswers = [];
   };
@@ -120,26 +124,73 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
         wrongAnswer = translate === undefined || evtTarget.innerText.match(/[а-я-,]/gi)?.join('') !== wordTranslation.wordTranslate.replace(/\s/g, '');
       };
 
-      setIsAnswered(true);
+      if (wrongAnswer) {
+        --lives;
+        wrongAnswers.push(wordTranslation);
+      } else {
+        correctAnswers.push(wordTranslation);
+      };
+
+      if (soundWaves.current) {
+        soundWaves.current.src = bgImage;
+        soundWaves.current.style.height = "100%";
+      }
+
+      if (wordContainer.current) {
+        wordContainer.current.classList.add("no-opacity");
+      }
+
+      if (word.current) {
+        word.current.classList.add("show-word")
+      }
+
+      if (button.current) {
+        button.current.classList.add("blue-btn")
+        button.current.innerHTML = `
+        <svg width="44" height="37" viewBox="0 0 44 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g clipPath="url(#clip0)">
+            <path d="M2.53446 17.0547H37.47L31.6828 12.0088C31.0371 11.4457 31.0346 10.5306 31.6773 9.96485C32.32 9.39901 33.3645 9.39691 34.0103 9.95993L42.6299 17.4756C42.6305 17.476 42.6309 17.4765 42.6314 17.4769C43.2755 18.04 43.2775 18.9581 42.6315 19.5231C42.631 19.5235 42.6305 19.524 42.63 19.5244L34.0104 27.04C33.3648 27.603 32.3202 27.601 31.6775 27.0351C31.0348 26.4694 31.0372 25.5543 31.683 24.9912L37.47 19.9453H2.53446C1.62337 19.9453 0.884834 19.2982 0.884834 18.5C0.884834 17.7017 1.62337 17.0547 2.53446 17.0547Z" fill="#4B2647" />
+          </g>
+          <defs>
+            <clipPath id="clip0">
+              <rect width="42.2304" height="37" fill="white" transform="matrix(-1 0 0 1 43.1152 0)" />
+            </clipPath>
+          </defs>
+        </svg>`;
+      }
 
       const wordTranslations = document.querySelectorAll<HTMLLIElement>(".minigames__translation-item");
       highlightWords(wordTranslations, wordTranslation);
-
-      setTimeout(() => {
-        if (wrongAnswer) {
-          setLives(prevLives => prevLives - 1);
-          wrongAnswers.push(wordTranslation);
-        } else {
-          correctAnswers.push(wordTranslation);
-        };
-
-        const updatedWords = gameWords.filter((word) => word.word !== wordTranslation.word);
-
-        setGameWords(updatedWords);
-        removeWordsHighlighting(wordTranslations, wordTranslation);
-      }, 800);
     };
   };
+
+  const onNextQuestionClick = (wordTranslation: WordsProps) => {
+    if (soundWaves.current) {
+      soundWaves.current.src = "/images/games/sound-waves.png";
+      soundWaves.current.style.height = "auto";
+    }
+
+    if (word.current) {
+      // word.current.style.opacity = "0.7";
+    }
+
+    if (button.current) {
+      button.current.innerHTML = `Не знаю`;
+      button.current.classList.remove("blue-btn");
+    }
+
+    if (word.current) {
+      word.current.classList.remove("show-word")
+    }
+
+    const wordTranslations = document.querySelectorAll<HTMLLIElement>(".minigames__translation-item");
+
+    const updatedWords = gameWords.filter((word) => word.word !== wordTranslation.word);
+
+    setGameWords(updatedWords);
+    removeWordsHighlighting(wordTranslations, wordTranslation);
+  }
+
 
   const translationWordIndex = useMemo(() => Math.floor(Math.random() * gameWords.length), [gameWords]);
   const translationWord: WordsProps = gameWords[translationWordIndex] || {};
@@ -155,6 +206,7 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
 
     return randomWord;
   }
+  const bgImage = new Image().src = urlBackend + translationWord.image;
 
   const translationsOnScreen: WordsProps[] = [
     getRandomWords(),
@@ -198,8 +250,7 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
                 if (audioChallenge.current !== null) {
                   audioChallenge.current.requestFullscreen();
                 }
-              }
-              }>
+              }}>
                 <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg" >
                   <path d="M5.57143 5.57143H0V7.42859H6.50001C7.01328 7.42859 7.42859 7.01328 7.42859 6.50001V0H5.57143V5.57143Z" fill="white" />
                   <path d="M19.8572 0H18V6.50001C18 7.01328 18.4153 7.42859 18.9286 7.42859H25.4286V5.57143H19.8572V0Z" fill="white" />
@@ -216,62 +267,30 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
                 </button>
               </div>
             </div>
-            <div className="audio-challenge__word no-opacity" onClick={() => playAudio(translationWord.audio)} ref={word}>
-              {!isAnswered ? 
-              <svg className="audio-challenge__sound-waves vawes-resizing" width="107" height="107" viewBox="0 0 107 107" fill="none" ref={soundWaves} xmlns="http://www.w3.org/2000/svg">
-                <g clipPath="url(#clip0)">
-                  <path d="M2.05813 45.3298C0.920743 45.3298 0 46.2506 0 47.3868V57.6751C0 58.8125 0.920743 59.7332 2.05813 59.7332C3.19434 59.7332 4.11509 58.8125 4.11509 57.6751V47.3868C4.11509 46.2506 3.19434 45.3298 2.05813 45.3298V45.3298Z" fill="#E3E3E3" fillOpacity="0.9" />
-                  <path d="M12.3535 36.9041C11.2173 36.9041 10.2954 37.826 10.2954 38.9622V67.4511C10.2954 68.5885 11.2173 69.5092 12.3535 69.5092C13.4898 69.5092 14.4117 68.5885 14.4117 67.4511V38.9622C14.4117 37.826 13.4898 36.9041 12.3535 36.9041Z" fill="#E3E3E3" fillOpacity="0.9" />
-                  <path d="M22.6348 22.6951C21.4986 22.6951 20.5767 23.6158 20.5767 24.752V82.3679C20.5767 83.5042 21.4986 84.4261 22.6348 84.4261C23.771 84.4261 24.6929 83.5042 24.6929 82.3679V24.752C24.6929 23.6158 23.771 22.6951 22.6348 22.6951Z" fill="#E3E3E3" fillOpacity="0.9" />
-                  <path d="M32.9234 32.9832C31.7872 32.9832 30.8652 33.9039 30.8652 35.0413V70.0213C30.8652 71.1575 31.7872 72.0794 32.9234 72.0794C34.0596 72.0794 34.9815 71.1575 34.9815 70.0213V35.0413C34.9815 33.9039 34.0596 32.9832 32.9234 32.9832Z" fill="#E3E3E3" fillOpacity="0.9" />
-                  <path d="M63.9891 28.0015C62.8529 28.0015 61.9321 28.9222 61.9321 30.0596V76.3546C61.9321 77.4908 62.8529 78.4116 63.9891 78.4116C65.1253 78.4116 66.0472 77.4908 66.0472 76.3546V30.0596C66.0472 28.9222 65.1253 28.0015 63.9891 28.0015Z" fill="#E3E3E3" fillOpacity="0.9" />
-                  <path d="M84.3658 30.9253C83.2295 30.9253 82.3076 31.8472 82.3076 32.9834V74.1366C82.3076 75.274 83.2295 76.1948 84.3658 76.1948C85.502 76.1948 86.4227 75.274 86.4227 74.1366V32.9834C86.4227 31.8472 85.502 30.9253 84.3658 30.9253Z" fill="#E3E3E3" fillOpacity="0.9" />
-                  <path d="M94.6543 41.2136C93.5181 41.2136 92.5962 42.1355 92.5962 43.2718V63.8484C92.5962 64.9846 93.5181 65.9065 94.6543 65.9065C95.7905 65.9065 96.7113 64.9846 96.7113 63.8484V43.2718C96.7113 42.1355 95.7905 41.2136 94.6543 41.2136Z" fill="#E3E3E3" fillOpacity="0.9" />
-                  <path d="M74.0777 14.4634C72.9403 14.4634 72.0195 15.3853 72.0195 16.5215V90.5978C72.0195 91.7351 72.9403 92.6559 74.0777 92.6559C75.2139 92.6559 76.1346 91.7351 76.1346 90.5978V16.5215C76.1346 15.3853 75.2139 14.4634 74.0777 14.4634V14.4634Z" fill="#E3E3E3" fillOpacity="0.9" />
-                  <path d="M43.2113 47.3867C42.075 47.3867 41.1543 48.3086 41.1543 49.4448V57.675C41.1543 58.8124 42.075 59.7331 43.2113 59.7331C44.3475 59.7331 45.2694 58.8124 45.2694 57.675V49.4448C45.2694 48.3086 44.3475 47.3867 43.2113 47.3867Z" fill="#E3E3E3" fillOpacity="0.9" />
-                  <path d="M104.942 46.6982C103.806 46.6982 102.884 47.619 102.884 48.7552V57.6589C102.884 58.7951 103.806 59.717 104.942 59.717C106.079 59.717 107.001 58.7951 107.001 57.6589V48.7552C107.001 47.619 106.079 46.6982 104.942 46.6982Z" fill="#E3E3E3" fillOpacity="0.9" />
-                  <path d="M53.3062 42.7751C52.1688 42.7751 51.248 43.6959 51.248 44.8321V61.5821C51.248 62.7183 52.1688 63.6402 53.3062 63.6402C54.4424 63.6402 55.3631 62.7183 55.3631 61.5821V44.8321C55.3631 43.6959 54.4424 42.7751 53.3062 42.7751Z" fill="#E3E3E3" fillOpacity="0.9" />
-                </g>
-                <defs>
-                  <clipPath id="clip0">
-                    <rect width="107" height="107" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-              :
-              <img src={urlBackend + translationWord.image} alt="word illustration"/>
-            }
+            <div className="audio-challenge__wrapper">
+              <div className="audio-challenge__word no-opacity" onClick={() => playAudio(translationWord.audio)} ref={wordContainer}>
+                <img className="audio-challenge__sound-waves vawes-resizing" src="/images/games/sound-waves.png" alt="sound or word illustrarion" ref={soundWaves} />
+                <span className="audio-challenge__word-answer" ref={word}>{translationWord.word}</span>
+              </div>
+              <ul className="minigames__translation-list">
+                {
+                  shuffleArray(translationsOnScreen).map((word: WordsProps, i: number) => {
+                    return (
+                      <li
+                        key={word.wordTranslate + i}
+                        className="minigames__translation-item"
+                        onClick={evt => onAnswer(translationWord, evt)}
+                      >
+                        <span>{i + 1}. {word.wordTranslate}</span>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+              <button className="btn audio-challenge__button" onClick={() => onNextQuestionClick(translationWord)} ref={button}>
+                Не знаю
+              </button>
             </div>
-            <ul className="minigames__translation-list">
-              {
-                shuffleArray(translationsOnScreen).map((word: WordsProps, i: number) => {
-                  return (
-                    <li
-                      key={word.wordTranslate + i}
-                      className="minigames__translation-item"
-                      onClick={evt => onAnswer(translationWord, evt)}
-                    >
-                      <span tabIndex={0}>{i + 1}. {word.wordTranslate}</span>
-                    </li>
-                  )
-                })
-              }
-            </ul>
-            <button className="btn audio-challenge__button" onClick={() => onAnswer(translationWord)}>
-              {!isAnswered ?
-                "Не знаю" :
-                <svg width="44" height="37" viewBox="0 0 44 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <g clip-path="url(#clip0)">
-                    <path d="M2.53446 17.0547H37.47L31.6828 12.0088C31.0371 11.4457 31.0346 10.5306 31.6773 9.96485C32.32 9.39901 33.3645 9.39691 34.0103 9.95993L42.6299 17.4756C42.6305 17.476 42.6309 17.4765 42.6314 17.4769C43.2755 18.04 43.2775 18.9581 42.6315 19.5231C42.631 19.5235 42.6305 19.524 42.63 19.5244L34.0104 27.04C33.3648 27.603 32.3202 27.601 31.6775 27.0351C31.0348 26.4694 31.0372 25.5543 31.683 24.9912L37.47 19.9453H2.53446C1.62337 19.9453 0.884834 19.2982 0.884834 18.5C0.884834 17.7017 1.62337 17.0547 2.53446 17.0547Z" fill="#4B2647" />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0">
-                      <rect width="42.2304" height="37" fill="white" transform="matrix(-1 0 0 1 43.1152 0)" />
-                    </clipPath>
-                  </defs>
-                </svg>
-              }
-            </button>
           </>
           :
           <div className='minigames__result'>
