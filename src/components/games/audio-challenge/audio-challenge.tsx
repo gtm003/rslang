cab6511f190f813e87f861006cc1f94370faa49d
alekseishkel <lexashkel@gmail.com>
2021-04-14 20:05:30 +0300

    feat: add response design

--- src/components/games/audio-challenge/audio-challenge.tsx
+++ src/components/games/audio-challenge/audio-challenge.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { WordsProps, GameProps } from "../../../common/ts/interfaces";
+import { NavLink } from 'react-router-dom';
 import { connect } from "react-redux";
 import { Loader } from "../../loader";
 import { ResultsGame } from '../resultsGame/resultsGame';
@@ -11,6 +12,7 @@ let correctAnswers: WordsProps[] = [];
 let wrongAnswers: WordsProps[] = [];
 let lives: number = 5;
 let onNextQuestionClicks: number = 0;
+let answers: number = 0;
 
 interface SavannahProps {
   words: WordsProps[]
@@ -36,6 +38,8 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
 
     correctAnswers = [];
     wrongAnswers = [];
+    answers = 0;
+    onNextQuestionClicks = 0;
   }, [words, group, page]);
 
   useEffect(() => {
@@ -75,11 +79,11 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
         return onAnswer(translationWord, translationsOnScreen[3].wordTranslate);
       case "5":
         return onAnswer(translationWord, translationsOnScreen[4].wordTranslate);
-      case " ":
+      case "Control":
         return playAudio(translationWord.audio);
       case "Enter":
-        ++onNextQuestionClicks;
-        return onNextQuestionClicks === 2 ? onNextQuestionClick(translationWord) : null;
+        return onNextQuestionClicks === 0 ? onAnswer(translationWord) : onNextQuestionClick(translationWord);
+
     }
   };
 
@@ -113,7 +117,6 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
     })
   };
 
-  let answers: number = 0;
 
   const onAnswer = (wordTranslation: WordsProps, translate?: React.MouseEvent | string): void => {
     ++answers;
@@ -161,6 +164,7 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
 
   const onNextQuestionClick = (wordTranslation: WordsProps) => {
     onNextQuestionClicks = 0;
+    answers = 0;
     if (soundWaves.current) {
       soundWaves.current.src = "/images/games/sound-waves.png";
       soundWaves.current.style.height = "auto";
@@ -252,11 +256,13 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
               </button>
               <div className="minigames__right-panel">
                 <Lives lives={lives} />
-                <button className="minigames__close" >
-                  <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.4347 14.022L0.532091 3.11904C-0.177435 2.40985 -0.177435 1.26318 0.532091 0.55399C1.24129 -0.155204 2.38795 -0.155204 3.09715 0.55399L14.0001 11.457L24.9028 0.55399C25.6123 -0.155204 26.7587 -0.155204 27.4679 0.55399C28.1774 1.26318 28.1774 2.40985 27.4679 3.11904L16.5652 14.022L27.4679 24.925C28.1774 25.6342 28.1774 26.7808 27.4679 27.49C27.1144 27.8438 26.6497 28.0215 26.1853 28.0215C25.7209 28.0215 25.2566 27.8438 24.9028 27.49L14.0001 16.5871L3.09715 27.49C2.74338 27.8438 2.279 28.0215 1.81462 28.0215C1.35024 28.0215 0.885857 27.8438 0.532091 27.49C-0.177435 26.7808 -0.177435 25.6342 0.532091 24.925L11.4347 14.022Z" fill="#CDCDCD" />
-                  </svg>
-                </button>
+                <NavLink to='/games'>
+                  <button className="minigames__close" >
+                    <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M11.4347 14.022L0.532091 3.11904C-0.177435 2.40985 -0.177435 1.26318 0.532091 0.55399C1.24129 -0.155204 2.38795 -0.155204 3.09715 0.55399L14.0001 11.457L24.9028 0.55399C25.6123 -0.155204 26.7587 -0.155204 27.4679 0.55399C28.1774 1.26318 28.1774 2.40985 27.4679 3.11904L16.5652 14.022L27.4679 24.925C28.1774 25.6342 28.1774 26.7808 27.4679 27.49C27.1144 27.8438 26.6497 28.0215 26.1853 28.0215C25.7209 28.0215 25.2566 27.8438 24.9028 27.49L14.0001 16.5871L3.09715 27.49C2.74338 27.8438 2.279 28.0215 1.81462 28.0215C1.35024 28.0215 0.885857 27.8438 0.532091 27.49C-0.177435 26.7808 -0.177435 25.6342 0.532091 24.925L11.4347 14.022Z" fill="#CDCDCD" />
+                    </svg>
+                  </button>
+                </NavLink>
               </div>
             </div>
             <div className="audio-challenge__wrapper">
@@ -280,7 +286,7 @@ const AudioChallengeRedux: React.FC<GameProps & SavannahProps> = ({ group, page 
                 }
               </ul>
               <button className="btn audio-challenge__button" ref={button} onClick={() => {
-                onNextQuestionClicks === 1 ? onAnswer(translationWord) : onNextQuestionClick(translationWord);
+                onNextQuestionClicks === 0 ? onAnswer(translationWord) : onNextQuestionClick(translationWord);
               }}>
                 Не знаю
               </button>
--- src/components/games/savannah/savannah.tsx
+++ src/components/games/savannah/savannah.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { WordsProps, GameProps } from "../../../common/ts/interfaces";
+import { NavLink } from 'react-router-dom';
 import { connect } from "react-redux";
 import { Loader } from "../../loader";
 import { Lives } from "../lives/lives";
@@ -209,11 +210,13 @@ const SavannahRedux: React.FC<GameProps & SavannahProps> = ({ group, page = -1, 
               </button>
               <div className="minigames__right-panel">
                 <Lives lives={lives} />
-                <button className="minigames__close" >
-                  <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.4347 14.022L0.532091 3.11904C-0.177435 2.40985 -0.177435 1.26318 0.532091 0.55399C1.24129 -0.155204 2.38795 -0.155204 3.09715 0.55399L14.0001 11.457L24.9028 0.55399C25.6123 -0.155204 26.7587 -0.155204 27.4679 0.55399C28.1774 1.26318 28.1774 2.40985 27.4679 3.11904L16.5652 14.022L27.4679 24.925C28.1774 25.6342 28.1774 26.7808 27.4679 27.49C27.1144 27.8438 26.6497 28.0215 26.1853 28.0215C25.7209 28.0215 25.2566 27.8438 24.9028 27.49L14.0001 16.5871L3.09715 27.49C2.74338 27.8438 2.279 28.0215 1.81462 28.0215C1.35024 28.0215 0.885857 27.8438 0.532091 27.49C-0.177435 26.7808 -0.177435 25.6342 0.532091 24.925L11.4347 14.022Z" fill="#CDCDCD" />
-                  </svg>
-                </button>
+                <NavLink to='/games'>
+                  <button className="minigames__close" >
+                    <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M11.4347 14.022L0.532091 3.11904C-0.177435 2.40985 -0.177435 1.26318 0.532091 0.55399C1.24129 -0.155204 2.38795 -0.155204 3.09715 0.55399L14.0001 11.457L24.9028 0.55399C25.6123 -0.155204 26.7587 -0.155204 27.4679 0.55399C28.1774 1.26318 28.1774 2.40985 27.4679 3.11904L16.5652 14.022L27.4679 24.925C28.1774 25.6342 28.1774 26.7808 27.4679 27.49C27.1144 27.8438 26.6497 28.0215 26.1853 28.0215C25.7209 28.0215 25.2566 27.8438 24.9028 27.49L14.0001 16.5871L3.09715 27.49C2.74338 27.8438 2.279 28.0215 1.81462 28.0215C1.35024 28.0215 0.885857 27.8438 0.532091 27.49C-0.177435 26.7808 -0.177435 25.6342 0.532091 24.925L11.4347 14.022Z" fill="#CDCDCD" />
+                    </svg>
+                  </button>
+                </NavLink>
               </div>
             </div>
             <div className="savannah__word flow-animation" ref={word} onAnimationEnd={() => { onAnswer(translationWord) }}>
